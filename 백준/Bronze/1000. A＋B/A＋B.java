afb9b7ecf5ad2f4d517858691eb295617b5a94c9
gyuminKim-kr <64451661+gyuminKim-kr@users.noreply.github.com>
2024-04-03 19:02:39 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		int num1,num2;
+		
+		Scanner in = new Scanner(System.in);
+		Scanner in1 = new Scanner(System.in);
+		
+		num1 = in.nextInt();
+		num2 = in.nextInt();
+		
+		System.out.println(num1+num2);
+	}
+}
