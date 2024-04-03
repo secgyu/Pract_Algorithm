27bfc434ffb815f5b805fbe9ada7ff2d4ec8a0b9
gyuminKim-kr <64451661+gyuminKim-kr@users.noreply.github.com>
2024-04-03 18:55:29 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 644 ms, Memory: 36960 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -11,6 +11,7 @@ interface Main {
                 A[i][j] = sc.nextInt();
             }
         }
+        
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
                 A[i][j] += sc.nextInt();
