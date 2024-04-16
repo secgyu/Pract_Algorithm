274b26203f773693ea53f00456b119cef0d248dd
gyuminKim-kr <64451661+gyuminKim-kr@users.noreply.github.com>
2024-04-16 11:37:45 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		String name;
+		Scanner in = new Scanner(System.in);
+		
+		name = in.next();
+		
+		System.out.println(name+"??!");
+	}
+}
