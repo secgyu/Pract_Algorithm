2db3ca0dfcd2f13f8cb670efd940eeb3e540b421
secgyu <64451661+secgyu@users.noreply.github.com>
2024-06-03 10:04:10 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+select id, email, first_name, last_name
+from developers
+where skill_code & (select code from skillcodes where name = 'Python')
+      or
+      skill_code & (select code from skillcodes where name = 'C#')
+order by id asc
