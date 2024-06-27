19d22d7bd41d5034ab92689fff9be95a12a3cf5d
secgyu <64451661+secgyu@users.noreply.github.com>
2024-06-27 22:48:36 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,20 +1,18 @@
 def solution(polynomial):
-    num = 0 
-    const = 0  
-    terms = polynomial.split(' + ')
-    
-    for term in terms:
-        if 'x' in term:
-            if term == 'x':
-                num += 1
-            else: num += int(term[:-1])
-        else: const += int(term)
-    
-    if num == 0: return str(const)
-    elif num == 1: result = 'x'
-    else: result = f'{num}x'
-    
-    if const != 0:
-        result += f' + {const}'
-    
-    return result
+    num = 0
+    const = 0
+
+    for i in polynomial.split(' + '):
+        if i.isdigit():
+            const += int(i)
+        else:
+            if i == 'x':
+                i = '1x'
+            num += int(i[:-1])
+
+    if num == 0:
+        return str(const)
+    elif num == 1:
+        return 'x + ' + str(const) if const != 0 else 'x'
+    else:
+        return f'{num}x + {const}' if const != 0 else f'{num}x' #
