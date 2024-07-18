be9e17d3e6c119e3853c0f94ad257163cfe12b81
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-18 20:56:20 +0900

    [Silver V] Title: 너의 평점은, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,19 @@
+grade_list = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+score_list = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+tot_credit = 0
+tot_score = 0
+
+for i in range(20):
+    a = list(map(str, input().split()))
+    credit = float(a[1])
+    if a[2] == 'P':
+        continue
+    else:
+        idx = grade_list.index(a[2])
+        score = score_list[idx]
+        tot_score += (credit * score)
+        tot_credit += credit
+
+avg_score = float(tot_score / tot_credit)
+print(f'{avg_score:.6f}')
