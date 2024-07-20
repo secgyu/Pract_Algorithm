e48cc09f2582179f7223b11f854871fde22d2349
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-20 17:16:15 +0900

    [Bronze II] Title: 화성 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,16 @@
+case = int(input())
+
+for _ in range(case):
+    mars = list(map(str, input().split()))
+    answer = 0
+    for i in range(len(mars)):
+        if i == 0:
+            answer += float(mars[i])
+        else:
+            if mars[i] == "#":
+                answer -= 7
+            elif mars[i] == "%":
+                answer += 5
+            elif mars[i] == "@":
+                answer *= 3
+    print("%0.2f" % answer)
