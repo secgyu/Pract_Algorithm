6fe332f6211bfa5097948a26b70fffba1aafce54
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-22 12:19:37 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,17 @@
+count = int(input())
+result = 0
+
+for _ in range(count):
+    a, b, c = map(int, input().split())
+
+    if a == b == c:
+        result = max(result, 10000+a*1000)
+    elif a == b:
+        result = max(result, 1000+a*100)
+    elif a == c:
+        result = max(result, 1000+a*100)
+    elif b == c:
+        result = max(result, 1000+b*100)
+    else:
+        result = max(result, 100*max(a, b, c))
+print(result)
