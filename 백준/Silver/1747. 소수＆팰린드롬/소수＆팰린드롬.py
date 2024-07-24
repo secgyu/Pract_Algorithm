91f88e60c7f98ce7f5224db1b8c1b87b0d4ec10a
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-24 22:23:20 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 864 ms, Memory: 112356 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,6 +1,6 @@
 import math
 N = int(input())
-A = [0] * (10000001)
+A = [0] * (2000001)
 
 for i in range(2, len(A)):
     A[i] = i
@@ -13,17 +13,9 @@ for i in range(2, int(math.sqrt(len(A)))+1):
 
 
 def isPalindrom(target):
-    tmp = list(str(target))
-
-    start = 0
-    end = len(tmp)-1
-
-    while (start < end):
-        if tmp[start] != tmp[end]:
-            return False
-        start += 1
-        end -= 1
-    return True
+    tmp = str(target)
+    return tmp == tmp[::-1]
+        
 
 
 i = N
