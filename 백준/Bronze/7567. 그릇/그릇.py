a3f78848b919d7aae92bbabecbd2090f973b2e86
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-25 14:59:33 +0900

    [Bronze II] Title: 그릇, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -0,0 +1,11 @@
+dish = input().strip()
+
+total_height = 10
+
+for i in range(1, len(dish)):
+    if dish[i] == dish[i-1]:
+        total_height += 5
+    else:
+        total_height += 10
+
+print(total_height)
