07c88194bf3a907b77da1801206fa4921b85ab23
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-25 16:06:33 +0900

    [Silver IV] Title: 다음 소수, Time: 1456 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+
+def is_prime(x):
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            return False
+    return True
+
+
+for i in range(N):
+    n = int(input())
+    while True:
+        if n == 0 or n == 1:
+            print(2)
+            break
+        if is_prime(n):
+            print(n)
+            break
+        else:
+            n += 1
