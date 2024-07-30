71de01803325e58ef947aa9b803b52e14d4fd4bc
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-30 21:57:45 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 216 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -8,15 +8,14 @@ def min_difficult(woods):
             left.append(woods[i])
         else:
             right.append(woods[i])
-    right.reverse()
-    arrange = left + right
+    
+    arrange = left + right[::-1]
 
     max_difficult = 0
 
-    for i in range(1, len(arrange)):
+    for i in range(len(arrange)):
         max_difficult = max(max_difficult, abs(arrange[i] - arrange[i-1]))
-    max_difficult = max(max_difficult, abs(arrange[0] - arrange[-1]))
-
+   
     return max_difficult
 
 
