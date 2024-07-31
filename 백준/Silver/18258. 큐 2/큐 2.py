7bae372dbc6cafee5b71f8dc85396184bca571f0
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-31 22:43:06 +0900

    [Silver IV] Title: 큐 2, Time: 1528 ms, Memory: 175124 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+queue = deque([])
+
+for _ in range(N):
+    command = input().split()
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    
+    elif command[0] == 'size':
+        print(len(queue))
+    
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
