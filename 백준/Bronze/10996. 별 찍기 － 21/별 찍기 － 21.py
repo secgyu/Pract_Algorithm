437530f01e9be71892cd7a05e48b346c289651e1
secgyu <64451661+secgyu@users.noreply.github.com>
2024-08-13 18:52:39 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+for _ in range(N):
+    for i in range(N):
+        if i % 2 == 0:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+    for i in range(N):
+        if i % 2 == 0:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print()
