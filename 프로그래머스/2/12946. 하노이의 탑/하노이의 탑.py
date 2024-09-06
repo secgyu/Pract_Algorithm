8d0a7452438aca3c402f61eada7084863f1430be
secgyu <64451661+secgyu@users.noreply.github.com>
2024-09-07 05:20:38 +0900

    [level 2] Title: 하노이의 탑, Time: 9.69 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    def hanoi(n, start, end, mid):
+        if n == 1:
+            answer.append([start, end])
+            return
+        hanoi(n-1, start, mid, end)
+        answer.append([start, end])
+        hanoi(n-1, mid, end, start)
+        
+    hanoi(n, 1, 3, 2)
+    return answer
