49ecd7992c43bda1ff863cb1da4c43df572fb366
secgyu <64451661+secgyu@users.noreply.github.com>
2024-09-08 11:20:19 +0900

    [level 2] Title: H-Index, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,5 @@
 def solution(citations):
     answer = 0
-    citations.sort()
-
-    for i in range(len(citations)):
-        if citations[i] >= len(citations) - i:
-            answer = len(citations) - i
-            break
+    citations.sort(reverse=True)
+    answer = max(map(min, enumerate(citations, start=1)))
     return answer
