b075d0d8c7bd121247740c55bf31a6e7c4e31396
secgyu <64451661+secgyu@users.noreply.github.com>
2024-09-08 12:20:27 +0900

    [level 2] Title: 가장 큰 수, Time: 1334.63 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,5 @@
-from functools import cmp_to_key
 
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(reverse=True, key=cmp_to_key(lambda x, y: int(x + y) - int(y + x)))
-    result = ''.join(numbers)
-    # 모든 숫자가 '0'일 경우 '0' 하나만 반환
-    return '0' if result[0] == '0' else result
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
