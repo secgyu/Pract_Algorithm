4e5e19018cd0f0daf201dd7bda52668943cd1cda
secgyu <64451661+secgyu@users.noreply.github.com>
2024-10-29 22:38:34 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -1,12 +1,12 @@
-angle1 = input()
-angle2 = input()
-angle3 = input()
-
-if int(angle1) == 60 and int(angle2) == 60 and int(angle3) == 60:
-    print('Equilateral')
-elif int(angle1) + int(angle2) + int(angle3) == 180 and int(angle1) == int(angle2) or int(angle2) == int(angle3) or int(angle1) == int(angle3):
-    print('Isosceles')
-elif int(angle1) + int(angle2) + int(angle3) == 180 and int(angle1) != int(angle2) and int(angle2) != int(angle3) and int(angle1) != int(angle3):
-    print('Scalene')
-elif int(angle1) + int(angle2) + int(angle3) != 180:
-    print('Error')
+a = int(input())
+b = int(input())
+c = int(input())
+if a+b+c == 180:
+    if a == b == c == 60:
+        print("Equilateral")
+    elif a == b or b == c or a == c:
+        print("Isosceles")
+    else:
+        print("Scalene")
+else:
+    print("Error")
