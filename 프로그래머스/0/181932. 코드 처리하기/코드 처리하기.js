54a546dfd987f94bbe9da0b6f50cfe56177de621
secgyu <64451661+secgyu@users.noreply.github.com>
2025-07-21 17:07:44 +0900

    [level 0] Title: 코드 처리하기, Time: 9.40 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,10 @@
+function solution(code) {
+    let ret = ''
+    let mode = 0
+    
+    for(let i = 0; i < code.length; i++){
+        if(mode == 0) code[i] == '1' ? (mode=1):(i%2==0&&(ret+=code[i]))
+        else code[i] =='1' ? (mode=0):(i%2!==0&&(ret+=code[i]))
+    }
+    return ret.length > 0 ? ret : "EMPTY";
+}
