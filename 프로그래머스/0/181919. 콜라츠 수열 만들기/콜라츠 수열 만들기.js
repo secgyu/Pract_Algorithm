02ceaf609eb00fd23bea306ae25540bff4da8c63
secgyu <64451661+secgyu@users.noreply.github.com>
2025-08-31 14:10:39 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,15 +1,12 @@
 function solution(n) {
-    var answer = [];
-    answer.push(n)
-    while(1){
-        if(n === 1) break
+    var answer = [n];
+    while(n !== 1){
         if(n % 2 === 0){
             n /= 2
-            answer.push(n)
         }else{
             n = 3*n+1
-            answer.push(n)
         }
+        answer.push(n)
     }
     return answer;
 }
