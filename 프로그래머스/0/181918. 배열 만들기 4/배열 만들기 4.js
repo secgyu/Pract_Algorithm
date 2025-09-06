2c9629b2056e4a0a7ff67657a999f30a95d73fd8
secgyu <64451661+secgyu@users.noreply.github.com>
2025-09-06 13:34:53 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.86 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+  const stk = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      i++;
+    } else if (stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      i++;
+    } else {
+      stk.pop();
+    }
+  }
+
+  return stk;
+}
